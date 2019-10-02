b9960b17288e290220cb55b99692bae361a84605
Alex <axllency@gmail.com>
2019-10-02 18:14:32 -0400

    Method to strip characters from article in Main

--- src/textAnalysis/Main.java
+++ src/textAnalysis/Main.java
@@ -3,7 +3,9 @@ package textAnalysis;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
@@ -24,5 +26,24 @@ public class Main {
 		}
 		System.out.println(in.nextLine());
 	}
-
+	
+	Set<Character> potato = new HashSet<Character>(); //every punctuation except for ' because of edge cases with apostraphe + s
+	//add relevant punctuation to potato (also change name of potato and refactor)
+	
+	public void stripPunc(String article) {//eventually replace string article with article object
+		//String article = thisArticle.getText();
+		Character tempCharacter;
+		for(int i = 0; i<article.length(); i++) { //for every character in the article
+			tempCharacter = article.charAt(i);//convert primitive char to Character
+			if(potato.contains(tempCharacter)) {//if char is punctuation
+				if(i==0) {//first char is punctuation
+					article = " " + article.substring(1); //replace punctuation with space
+				} else if(i+1<article.length()){//any char but first or last is punctuation
+					article = article.substring(0, i) + " " + article.substring(i+1);
+				} else {//last char is punctuation
+					article = article.substring(0, i) + " ";
+				}
+			}
+		}
+	}
 }
